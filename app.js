7239dff885fa537ea8d917c8a413fb525c2503af
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2021-09-17 22:52:30 +0400

    Update: use api middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const api = require('./api');
+
 require('dotenv').config();
 
 const app = express();
@@ -15,12 +17,6 @@ app.use(express.json());
 app.get('/version', (req, res) => res.send(process.env['version']));
 app.use('/health-check', (req, res) => res.sendStatus(200));
 app.use(/^\/$/, (req, res) => res.sendStatus(200));
-
-app.get('/v1', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
-
+app.use('/api/v1', api);
 
 module.exports = app;
