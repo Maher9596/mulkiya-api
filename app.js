c752cd29ff03a1caae05fe944385d42b765aaa3c
Maher Alkendi <maheralkendi@Mahers-MacBook-Air.local>
2021-10-01 21:15:51 +0400

    Add: OCR model logic

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ require('dotenv').config();
 const app = express();
 
 // === Middleware ===
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
 app.get('/version', (req, res) => res.send(process.env['version']));
 app.use('/health-check', (req, res) => res.sendStatus(200));
 app.use(/^\/$/, (req, res) => res.sendStatus(200));
--- packages/google-vision/index.js
+++ packages/google-vision/index.js
@@ -1,77 +1,51 @@
-// By default, the client will authenticate using the service account file
-// specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
-// the project specified by the GCLOUD_PROJECT environment variable. See
-// https://cloud.google.com/docs/authentication/getting-started#setting_the_environment_variable
+// // By default, the client will authenticate using the service account file
+// // specified by the GOOGLE_APPLICATION_CREDENTIALS environment variable and use
+// // the project specified by the GCLOUD_PROJECT environment variable. See
+// // https://cloud.google.com/docs/authentication/getting-started#setting_the_environment_variable
 
-const fs = require("fs").promises;
+const vision = require('@google-cloud/vision');
 
-async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
+class OcrModel {
 
-    function extractDescription(texts) {
-      let document = '';
-      texts.forEach(text => {
-        document += text.description || '';
-      });
-      return document.toLowerCase();
-    }
-
-    async function extractDescriptions(filename, index, response) {
-      if (response.textAnnotations.length) {
-        const words = await extractDescription(response.textAnnotations);
-
-        const regex = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{4}\/[0-9]{2}\/[0-9]{2}/g
-        const regex2 = /نوع المركبة.*\s*:\s*.*/g
-        console.log(words);
-        console.log(words.match(regex));
-        console.log(words.match(regex2));
-        // x.match(/نوع المركبة.*\s*:\s*.*/g)
-
-      } else {
-        console.log(`${filename} had no discernable text.`);
+  labelDates(extractDescriptionTextArray) {
+    if (extractDescriptionTextArray[0] > extractDescriptionTextArray[1]) {
+      return {
+        ExpiryDate: extractDescriptionTextArray[0],
+        StartDate: extractDescriptionTextArray[1]
+      }
+    } else {
+      return {
+        ExpiryDate: extractDescriptionTextArray[1],
+        StartDate: extractDescriptionTextArray[0]
       }
     }
-  
-    // Performs label detection on the image file
-    // const [result] = await client.labelDetection('./img/1.jpeg');
-    // const labels = result.labelAnnotations;
-    // console.log('Labels:');
-    // labels.forEach(label => console.log(label.description));
-    async function getTextFromFiles(index, inputFiles) {
-      // Read all of the given files and provide request objects that will be
-      // passed to the Cloud Vision API in a batch request.
-      const requests = await Promise.all(
-        inputFiles.map(async filename => {
-          const content = await fs.readFile(filename);
-          console.log(` 👉 ${filename}`);
-          return {
-            image: {
-              content: content.toString('base64'),
-            },
-            features: [{type: 'TEXT_DETECTION'}],
-          };
-        })
-      );
-    
-      // Make a call to the Vision API to detect text
-      const results = await client.batchAnnotateImages({requests});
-      const detections = results[0].responses;
-      await Promise.all(
-        inputFiles.map(async (filename, i) => {
-          const response = detections[i];
-          if (response.error) {
-            console.info(`API Error for ${filename}`, response.error);
-            return;
-          }
-          await extractDescriptions(filename, index, response);
-        })
-      );
-    }
-    getTextFromFiles(1, ["./img/1.jpeg"]);
   }
 
-module.exports = quickstart;
+  extractDescription(extractedText) {
+    // const dateRegex = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{4}\/[0-9]{2}\/[0-9]{2}/g;
+    const dateRegex = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}|[0-9]{4}\/[0-9]{2}\/([0-9]{2}|[0-9]{1})/g;
+    // const regex2 = /\nنوع المركبة.*\s*:\s*.*/g
+    return this.labelDates(extractedText.match(dateRegex));
+  }
+
+  extractTextFromResult(googleVisionPayload) {
+    return this.extractDescription(googleVisionPayload[0].fullTextAnnotation.text);
+  }
+
+  async getTextFromImage(base64Image) {
+    try {
+      const client = new vision.ImageAnnotatorClient();
+      const results = await client.textDetection({
+        image: {
+          content: base64Image
+        }
+      });
+      return this.extractTextFromResult(results);
+    } catch (error) {
+      console.log("Failed to get test from image");
+      console.log("Error message", error);
+    }
+  }
+}
+
+module.exports = OcrModel;
